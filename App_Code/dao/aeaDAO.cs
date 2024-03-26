eb9421e026455184940e3cccf1b1c6b18d5ad4a3
arnysc99@hotmail.com <arnysc99@hotmail.com>
2024-03-25 21:33:58 -0500

    se agregi aeaDAO 25-03-24

---
+++ App_Code/dao/aeaDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de aeaDAO
+/// </summary>
+public class aeaDAO
+{
+    public aeaDAO()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
